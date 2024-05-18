16958b2d31281ac7053422fdc60cd1dc50978cff
kvarun007 <kandula.varun.79@gmail.com>
2024-05-18 13:18:15 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
+import Footer from './components/fotter';
+import socials from './components/socials';
+import Socials from './components/socials';
+import Profile from './components/profile';
+import About from './components/about';
+import SiteRoutes from './routes/siteRoutes';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import Projects from './components/projects';
+import Products from './components/products';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <BrowserRouter><Navbar/>
+    <div className='max-w-96 mx-auto bg-white  px-3 pt-3 pb-2 mt-2 rounded-lg'>
+      <Routes><Route path="/" element={<Profile />}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/projects" element={<Projects/>}/>
+      <Route path="/products" element={<Products/>}/></Routes>
+      
+      
+      
+      {/*<Profile/>*/}
+      {/*<About/>*/}
+      <Socials/>
+      <Footer/>
     </div>
+    </BrowserRouter>
+    
+    </>
+    
+    
   );
 }
 
